0681369f9092a0a8f8f27e230b4e0fd9fe36cdd5
eperez14 <eperez14@terpmail.umd.edu>
2025-09-18 19:10:31 -0400

    new route using about.html

--- app.py
+++ app.py
@@ -5,8 +5,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World from Emma Perez!'
+    return 'Hello World from Emma Perez! I am adding my first code change'
 
+@app.route('/hello')
+def hello():
+    return render_template("hello.html")
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run()
