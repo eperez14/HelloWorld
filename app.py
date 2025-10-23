1b2bf901ace2cc17dbd9911a28618a4dae622def
Emma S Perez <eperez14@terpmail.umd.edu>
2025-10-23 17:28:56 -0400

    new module base, contact, and fav course html

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -22,6 +22,22 @@ def about_css():
 @app.route('/greeting')
 def greeting():
     return render_template('greeting.html')
+
+@app.route('/favorite-course')
+def favorite_course():
+    subject = request.args.get('subject')
+    course_number = request.args.get('course_number')
+    return render_template('favorite-course.html', subject=subject, course_number=course_number)
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        email = request.form['email']
+        message = request.form['message']
+        return render_template('contact.html', submitted=True, first_name=first_name, last_name=last_name, email=email, message=message)
+    return render_template('contact.html', submitted=False)
 
 if __name__ == '__main__':
     app.run()
